9f853b557ff37422d2238b5ec8e07300ba0da54a
Chen Li <leeson9394@gmail.com>
2017-03-02 16:58:34 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['firewall']['allow_ssh'] = true
 default['firewall']['firewalld']['permanent'] = true
-default['sre-challenge']['open_ports'] = 80
+default['sre-challenge']['open_ports'] = [22, 80, 443]
